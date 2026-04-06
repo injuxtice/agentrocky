497cce2cf76a251a690a4192009a8e65c0c6b3f1
sneha <30837089+itmesneha@users.noreply.github.com>
2026-04-07 01:26:03 +0800

    Initial Commit

---
+++ agentrocky/agentrockyApp.swift
@@ -0,0 +1,17 @@
+//
+//  agentrockyApp.swift
+//  agentrocky
+//
+//  Created by Sneha S on 7/4/26.
+//
+
+import SwiftUI
+
+@main
+struct agentrockyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
